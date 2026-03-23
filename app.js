ca4ba96360ff45f144771e85edc1df60d8932671
rachelhuynh-hub <Rachelhuynh@M365x40469006.onmicrosoft.com>
2026-03-23 12:54:13 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
-console.log("Hello from Cloudflare CDN");
-alert("JS loaded OK");
+(function () {
+  console.log("✅ JS loaded from Cloudflare Pages");
+ 
+  document.addEventListener("DOMContentLoaded", function () {
+    console.log("✅ DOM ready");
+ 
+    const div = document.createElement("div");
+    div.innerText = "JS loaded successfully ✅";
+    div.style.padding = "10px";
+    div.style.background = "#e6ffe6";
+ 
+    document.body.appendChild(div);
+  });
+})();
